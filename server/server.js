1219d0312c670d6fbf164579cacf0a542f14b2a5
ArianGhatrehSamani <arianghatreh@gmail.com>
2025-01-31 22:45:04 +0330

    changed docker files of client and server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const pg = require("pg-promise");
 app.get("/", (req, res) => {
   console.log("Hello there!");
   res.send("General Kenobi...");
